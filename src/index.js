3a756d1824649aa30ca64150b91963ece05c147b
Ikraam <ikraamghoor@gmail.com>
2020-07-24 16:08:31 +0200

    Can add todo to project and fix modals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './style.scss';
 import Project from './projects';
 import Todo from './todos';
 
-MicroModal.init();
 const projectDiv = document.querySelector('#projects');
 const createProjectBtn = document.querySelector('#create-project');
 console.log(createProjectBtn);
@@ -23,7 +22,7 @@ const createProject = () => {
 
 const createTodoForm = () => {
   const form = document.createElement('form');
-  const inputs = ['title', 'description', 'dueDate', 'priority'];
+  const inputs = ['title', 'description', 'dueDate', 'priority', 'project'];
   inputs.forEach(input => {
     const element = document.createElement('input');
     element.textContent = input;
@@ -36,40 +35,57 @@ const createTodoForm = () => {
   button.addEventListener('click', createTodo);
   document.querySelector('#todo-form').appendChild(form);
 };
-const createTodo = () => {
-  const project = defaultProject;
+const createTodo = (event) => {
+  event.preventDefault();
+  const projectName = document.querySelector('.todo-project').value;
+  const project = projectList.filter(item => item.title === projectName);
   const title = document.querySelector('.todo-title').value;
-  const description = document.querySelector('.todo-decription').value;
+  const description = document.querySelector('.todo-description').value;
   const dueDate = document.querySelector('.todo-dueDate').value;
   const priority = document.querySelector('.todo-priority').value;
   const newTodo = new Todo(title, description, dueDate, priority);
-  project.todoList.push(newTodo);
+  console.log(newTodo);
+  console.log(project);
+  project[0].todoList.push(newTodo);
+  renderProjects();
 };
 
 const getTodoList = (project) => {
   const ul = document.createElement('ul');
   project.todoList.forEach(todo => {
-    const li = document.createElement('li');
+    const li = document.createElement('button');
     li.textContent = todo.title;
+    li.setAttribute('data-micromodal-trigger', 'modal-1');
     ul.appendChild(li);
   });
   return ul;
 };
+
+const cleanPage = () => {
+  while (projectDiv.firstChild) {
+    projectDiv.removeChild(projectDiv.lastChild);
+  }
+};
+
 const renderProjects = () => {
+  cleanPage();
   projectList.forEach(project => {
     const p = document.createElement('p');
-    p.innerHTML = `<button data-micromodal-trigger="modal-1">Click here to open the modal!</button>`
-    // const newTodoBtn = document.createElement('button');
-    // newTodoBtn.setAttribute("data-micromodal-trigger", "modal-1");
-    // newTodoBtn.textContent = '+-';
-    // p.textContent = project.title;
-    // p.appendChild(newTodoBtn);
-    // projectDiv.appendChild(newTodoBtn);
-    // newTodoBtn.addEventListener('click', createTodoForm);
+    p.textContent = project.title;
     projectDiv.appendChild(p);
     projectDiv.appendChild(getTodoList(project));
   });
 };
 
+// document.querySelector('.create-todo-btn').addEventListener('click', createTodoForm);
+createTodoForm();
+
 createProjectBtn.addEventListener('click', createProject);
 renderProjects();
+
+const li = document.createElement('button');
+li.textContent = 'asdfasdf';
+li.setAttribute('data-micromodal-trigger', 'modal-1');
+document.body.appendChild(li);
+
+MicroModal.init();
